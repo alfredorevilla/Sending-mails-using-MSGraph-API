9c5d441ffddcae538b3980c8e512e3aaeeb58ddc
Alfredo Revilla <alfredorevilla@gmail.com>
2020-12-31 19:23:40 -0500

    Initial commit.
    Complete.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+
+namespace Sending_mails_using_Graph_API
+{
+    class Program
+    {
+        private static readonly string clientId = "<value>";
+        private static readonly string tenantID = "<value>";
+        private static readonly string clientSecret = "<value>";
+
+        static async Task Main(string[] args)
+        {
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+                .Create(clientId)
+                .WithTenantId(tenantID)
+                .WithClientSecret(clientSecret)
+                .Build();
+
+            ClientCredentialProvider authProvider = new ClientCredentialProvider(confidentialClientApplication);
+
+            GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+            var message = new Message
+            {
+                Subject = "Meet for lunch?",
+                Body = new ItemBody
+                {
+                    ContentType = BodyType.Text,
+                    Content = "The new cafeteria is open."
+                },
+                ToRecipients = new List<Recipient>()
+                {
+                    new Recipient
+                    {
+                        EmailAddress = new EmailAddress
+                        {
+                            Address = "<value>";
+                        }
+                    }
+                },
+                CcRecipients = new List<Recipient>()
+                {
+                    new Recipient
+                    {
+                        EmailAddress = new EmailAddress
+                        {
+                            Address = "<value>";
+                        }
+                    }
+                }
+            };
+
+            var saveToSentItems = true;
+            await graphClient
+                .Users["shared1@alfredorevillamsft1.OnMicrosoft.com"]
+              .SendMail(message, saveToSentItems)
+              .Request()
+              .PostAsync();
+
+            Console.WriteLine(0);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Sending mails using (MS) Graph API
+
+## Requirements
+[Download and install .NET Core 3.1 runtime or SDK](https://dotnet.microsoft.com/download/dotnet-core/3.1)
+
+
+## Setup
+Open Program.cs and replace all `<value`> instances found between double quotes.
+
+## Run
+
+From command prompt run:
+
+`dot net run`
+
+(C) Copyright 2020 Alfredo O. Revilla
